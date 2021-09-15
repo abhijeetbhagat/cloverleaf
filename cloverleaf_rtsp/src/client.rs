f8c2a9ea420b997070b7b6815ab08da88c025f19
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-15 19:45:32 +0530

    add a member to hold a callback for packet reception

--- cloverleaf_rtsp/src/client.rs
+++ cloverleaf_rtsp/src/client.rs
@@ -1,13 +1,22 @@
+use crate::RTPPacket;
 use insight::connection::RtspConnection;
 
 pub struct RTSPSource {
     conn: RtspConnection,
+    callback: Box<dyn Fn(RTPPacket)>,
 }
 
 impl RTSPSource {
     pub fn new<S: Into<String>>(url: S) -> Result<Self, String> {
         Ok(RTSPSource {
-            conn: RtspConnection::new(url).unwrap(),
+            conn: RtspConnection::new(url)?,
+            callback: Box::new(|p| {
+                println!("{:?}", p);
+            }),
         })
+    }
+
+    pub fn on_packet<C: Fn(RTPPacket) + 'static>(&mut self, callback: C) {
+        self.callback = Box::new(callback);
     }
 }
