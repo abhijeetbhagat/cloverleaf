85a42d9fbea2cdcb0f082b604f0207628f7e7b78
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-27 18:19:14 +0530

    add a streamer struct

---
+++ cloverleaf_core/src/streamer.rs
@@ -0,0 +1,33 @@
+use cloverleaf_rtsp::client::RTSPSource;
+use cloverleaf_rtsp::RTPPacket;
+use std::sync::Arc;
+use std::sync::RwLock;
+use tokio::sync::broadcast::Sender;
+
+pub struct Streamer {
+    tx: Arc<RwLock<Sender<RTPPacket>>>,
+}
+
+impl Streamer {
+    pub fn new(tx: Arc<RwLock<Sender<RTPPacket>>>) -> Self {
+        Streamer { tx }
+    }
+
+    pub async fn run(self) {
+        let mut source = RTSPSource::new("rtsp://localhost:8554/test1").unwrap();
+        let tx = Arc::clone(&self.tx);
+        loop {
+            let packet = source.get_packet();
+            match packet {
+                Some(packet) => {
+                    let tx = tx.read().unwrap();
+                    // broadcast to viewers
+                    tx.send(packet).unwrap();
+                }
+                _ => {}
+            }
+        }
+    }
+
+    pub async fn on_packet(&self, packet: RTPPacket) {}
+}
