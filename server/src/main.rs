b5dc3d14c7f41f2a49f50076cc7e9f5d7f8f205d
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-27 20:16:27 +0530

    use rocket_cors crate to handle CORS problems

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,39 +2,15 @@
 extern crate rocket;
 use cloverleaf_api::connect::{initiate, recv_answer};
 use cloverleaf_api::state::CloverLeafState;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> rocket::fairing::Info {
-        Info {
-            name: "Add CORS headers to response",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "GET, POST, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[launch]
 fn entry() -> _ {
     let state = CloverLeafState::new().unwrap();
 
+    let cors = rocket_cors::CorsOptions::default().to_cors().unwrap();
+
     rocket::build()
         .manage(state)
-        // .mount("/", [initiate])
         .mount("/", routes![initiate, recv_answer])
-        .attach(CORS)
+        .attach(cors)
 }
