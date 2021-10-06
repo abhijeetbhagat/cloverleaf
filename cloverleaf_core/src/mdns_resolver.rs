be386b7128ae9850516d9ffc2652cc2ab1efa3c4
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-10-06 20:18:35 +0530

    wip: add callback that completes hostname resolution

--- cloverleaf_core/src/mdns_resolver.rs
+++ cloverleaf_core/src/mdns_resolver.rs
@@ -1,21 +1,31 @@
 use std::ffi::CString;
 
+use libc::c_char;
 use libnice_sys::{
-    g_resolver_get_default, g_resolver_lookup_by_name_async, gpointer, GAsyncResult, GObject,
+    g_inet_address_to_string, g_resolver_get_default, g_resolver_lookup_by_name_async,
+    g_resolver_lookup_by_name_finish, gchar, gpointer, strcpy, GAsyncResult, GError, GInetAddress,
+    GObject,
 };
 
 pub fn mdns_resolver(addr: &str) -> Result<String, String> {
     unsafe {
         let resolver = g_resolver_get_default();
+        let result: *mut c_char = std::ptr::null_mut();
+        let addr = CString::new(addr);
         g_resolver_lookup_by_name_async(
             resolver,
-            CString::new(addr).unwrap().as_ptr(),
+            addr.unwrap().as_ptr(),
             std::ptr::null_mut(),
             Some(callback),
-            std::ptr::null_mut(),
+            result as gpointer,
         );
+        if result.is_null() {
+            println!("result is null");
+            Err("there was an error resolving mdns address".into())
+        } else {
+            Ok(CString::from_raw(result as *mut _).into_string().unwrap())
+        }
     }
-    Err("there was an error resolving mdns address".into())
 }
 
 // static void janus_sdp_mdns_resolved(GObject *source_object, GAsyncResult *res, gpointer user_data) {
@@ -24,4 +34,11 @@ unsafe extern "C" fn callback(
     res: *mut GAsyncResult,
     user_data: gpointer,
 ) {
+    let resolver = g_resolver_get_default();
+    let mut error: *mut GError = std::ptr::null_mut();
+    let list = g_resolver_lookup_by_name_finish(resolver, res, std::ptr::addr_of_mut!(error));
+    let resolved = g_inet_address_to_string((*list).data as *mut GInetAddress);
+    let mut user_data: *mut gchar = user_data as *mut _;
+    user_data = libnice_sys::g_malloc0(libnice_sys::strlen(resolved) + 1) as *mut _;
+    strcpy(user_data as *mut _, resolved);
 }
