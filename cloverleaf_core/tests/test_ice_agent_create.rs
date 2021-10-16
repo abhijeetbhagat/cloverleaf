a60189d0f3e65df69467731af19b719d9c88eb9c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-10-16 09:21:34 +0530

    add a test for gathering candidates

--- cloverleaf_core/tests/test_ice_agent_create.rs
+++ cloverleaf_core/tests/test_ice_agent_create.rs
@@ -2,7 +2,18 @@ use cloverleaf_core::IceAgent;
 use glib::MainContext;
 
 #[test]
-fn test_ice_agent_create() {
+fn test_create() {
     let agent = IceAgent::new(MainContext::new());
     assert!(agent.is_ok());
+}
+
+#[test]
+fn test_getting_local_candidates() {
+    let agent = IceAgent::new(MainContext::new());
+    assert!(agent.is_ok());
+    let agent = agent.unwrap();
+    let lcands = agent.get_local_candidates();
+    assert!(lcands.is_ok());
+    let lcands = lcands.unwrap();
+    assert!(lcands.len() > 0);
 }
