524e05e640dd8adeed07e7cd4f57188125f41a33
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-10-16 13:38:03 +0530

    impl Display for Transport

--- cloverleaf_core/src/transport.rs
+++ cloverleaf_core/src/transport.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Transport {
     Udp,
@@ -11,6 +13,16 @@ impl From<&str> for Transport {
             "UDP" => Self::Udp,
             "TCP" => Self::Tcp,
             _ => Self::Invalid,
+        }
+    }
+}
+
+impl Display for Transport {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Transport::Udp => write!(f, "UDP"),
+            Transport::Tcp => write!(f, "TCP"),
+            Transport::Invalid => Err(std::fmt::Error),
         }
     }
 }
